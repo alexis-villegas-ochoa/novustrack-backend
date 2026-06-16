045af6628116c7183e9514511b8fc1afd3c8f32d
Alexis Osvaldo Villegas Ochoa <alexisvillegasochoa+github@gmail.com>
2026-06-15 19:51:17 -0600

    feat: add User model, Spring Security, and DTO validation

---
+++ src/main/java/io/novustrack/backend/dto/RegisterRequest.java
@@ -0,0 +1,23 @@
+package io.novustrack.backend.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, message = "Password must be at leats 8 characters long for security")
+    private String password;
+}
---
+++ src/main/java/io/novustrack/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package io.novustrack.backend.repository;
+
+import io.novustrack.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsUserByEmail(String email);
+}
